f62d9212c7227965f6572e765f24d3bb2f4dfe54
Gomez, Bethy <bethygomez@usf.edu>
2019-09-19 20:14:34 -0400

    Deliverable 3 - Methods (v2)

--- Methodv2/Program.cs
+++ Methodv2/Program.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿/*
+ Author: Bethy Gomez
+ Date: 9/19/2019
+ Description: Deliverable 3 - Methods
+ */
+
+using System;
 
 namespace Methodv2
 {
@@ -6,7 +12,22 @@ namespace Methodv2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+            Console.WriteLine("Please enter your name...");
+            GreetUser(); // GreetUser method that will display the user output
+            // To pause program before closing
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit the program.");
+            Console.ReadKey(true);
+        } // End of main
+
+       public static void GreetUser() // Method to display "Hello users name!"
+        {
+            string username = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("Hello " + username + "!");
+            // Something extra to display the current date/time
+            Console.WriteLine("It is currently: " + System.DateTime.Now);
+        } // End of GreetUser
+
+    } // End of Class
+} // End of Namespace
